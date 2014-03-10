41969b7856182796a56824337536f4c50b27e679
Shaun Sharples <shaun.sharples@gmail.com>
2014-03-10 09:02:39 +0200

    started using conduit

--- Main.hs
+++ Main.hs
@@ -1,25 +1,37 @@
 module Main where
 
+import Control.Monad.Trans
+
 import Data.Binary
+import Data.Conduit
+import Data.Conduit.List as CL
 import Network.Pcap
 import Pcap
 import Quote
 
-pcaploop :: PcapHandle -> Link -> IO ()
-pcaploop h l = do
-    mp <- nextPcap h l
+pcapSrc :: PcapHandle -> Link -> Source IO Pcap
+pcapSrc h l = do
+    mp <- liftIO $ nextPcap h l
     case mp of
         Just p -> do
-            -- putStrLn $ pretty p
-            case decodeOrFail (_payload p) of
-                Right (_, _, q@Quote{}) -> print q
-                _ -> return ()
-            pcaploop h l
+            yield p
+            pcapSrc h l
         Nothing ->
             return ()
+
+pcapDecode :: Monad m => Conduit Pcap m Quote
+pcapDecode = CL.concatMap decodePcap
+    where
+        decodePcap p =
+            case decodeOrFail (_payload p) of
+                Right (_, _, q@Quote{}) -> [q]
+                _ -> []
+
+printer :: Show s => Sink s IO ()
+printer = CL.mapM_ $ putStrLn . show
 
 main :: IO ()
 main = do
     h <- openOffline "mdf-kospi200.20110216-0.pcap"
     dl <- Network.Pcap.datalink h
-    pcaploop h dl
+    pcapSrc h dl $$ pcapDecode =$ printer
