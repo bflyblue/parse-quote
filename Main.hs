d1fe08f5b31e23aef582e71d355fb735ca87aa33
Shaun Sharples <shaun.sharples@gmail.com>
2014-03-12 12:38:55 +0200

    cleanup time functions a bit

--- Main.hs
+++ Main.hs
@@ -21,7 +21,38 @@ import Pcap
 import Quote
 import Time
 
-pcapOffline :: MonadIO m => Conduit String m Pcap
+-- |WINDOW is hard-coded to 3 seconds for now to match requirements
+window :: Int
+window = 300
+
+-- |Parse and print quote data in pcap files specified on the command-line.
+main :: IO ()
+main = do
+    execParser opts >>= parse
+    where
+        opts = info (helper <*> options)
+             (fullDesc <> progDesc "Parse and print quote messages from market data in FILES")
+
+data Options = Options
+    { _files    :: [String] -- ^ Files to parse (in order)
+    , _reorder  :: Bool     -- ^ Reorder quotes (within 'window' second window)
+    }
+    deriving Show
+
+options :: Parser Options
+options = Options
+    <$> arguments str   (metavar "FILES...")
+    <*> switch          (short 'r' <> long "reorder" <> help "Reorder according to quote accept time")
+
+-- |Parse pcap file and print quotes. Reordering is done across files, so files should
+-- be specified in order.
+parse :: Options -> IO ()
+parse o = do
+    let dec = if _reorder o then pcapDecode =$= quoteReorder window else pcapDecode
+    evalStateT (CL.sourceList (_files o) $$ pcapOffline =$= dec =$ quotePrinter) PQ.empty
+
+-- |Conduit that takes filepaths of offline pcap data and yields pcap packets.
+pcapOffline :: MonadIO m => Conduit FilePath m Pcap
 pcapOffline = awaitForever $ \filepath -> do
     ph <- liftIO $ openOffline filepath
     dl <- liftIO $ Network.Pcap.datalink ph
@@ -36,30 +67,44 @@ pcapOffline = awaitForever $ \filepath -> do
                 Nothing ->
                     return ()
 
+-- |Conduit that takes pcap packets, decodes the payload and yields quotes with it's
+-- associated capture time.
 pcapDecode :: Monad m => Conduit Pcap m (Time, Quote)
 pcapDecode = CL.concatMap decodePcap
     where
         decodePcap p =
             case decodeOrFail (_payload p) of
-                Right (_, _, q@Quote{}) -> [(pcapTime p q, q)]
+                Right (_, _, q@Quote{}) -> [(adjustPcapTime p q, q)]
                 _ -> []
 
-        pcapTime p q =
-            let t = hdrTime . _pcapHdr $ p
-                sec = fromIntegral $ t `div` 1000000 `mod` 60
-                hsec = fromIntegral $ t `div` 10000 `mod` 100
-                Time h m s _ = parseTime (_acceptTime q)
-                (h', m') = (h + (m + 1) `div` 60 , (m + 1) `mod` 60)
-            in snd $ min (abs (sec - s), Time h m sec hsec) (abs (60 + sec - s), Time h' m' sec hsec)
+-- |Ugly function that tries to guess capture time.
+-- pcap time is in Unixtime (UCT) but Accept time is in Localtime.
+-- We assume they're no more than 3 seconds apart;
+-- here we try round 30 seconds up or down to try match Accept time.
+adjustPcapTime :: Pcap -> Quote -> Time
+adjustPcapTime packet quote =
+    let accept = parseTime . _acceptTime $ quote
+        capture = fromUnixTime . hdrTime . _pcapHdr $ packet
+        Time ah am as ahs = accept
+        Time ch cm cs chs = capture
+        adjustLow = Time ah am cs chs
+        adjustHi  = addTime adjustLow 6000
+        delta t = (abs $ diffTime t accept, t)
+    in snd $ min (delta adjustLow) (delta adjustHi)
+
+dumpPretty :: (MonadIO m, Pretty a) => Conduit a m a
+dumpPretty = CL.mapM $ \a -> do
+    liftIO . putStrLn . pretty $ a
+    return a
 
 type PQueue = PQ.MinPQueue Time (Time, Quote)
 
 quoteReorder :: Monad m => Int -> Conduit (Time, Quote) (StateT PQueue m) (Time, Quote)
-quoteReorder window = do
+quoteReorder w = do
     ma <- await
     case ma of
         Just (t, q) -> do
-            flush $ (>window) . diffTime t
+            flush $ (>w) . diffTime t
             lift $ modify $ PQ.insert (parseTime $ _acceptTime q) (t, q)
             quoteReorder window
         Nothing -> do
@@ -85,25 +130,3 @@ quotePrinter = CL.mapM_ $ \(t, q) -> do
     where
         pq (p', q') = p' <> "@" <> q'
 
-data Options = Options
-    { _files    :: [String]
-    , _reorder  :: Bool
-    }
-    deriving Show
-
-options :: Parser Options
-options = Options
-    <$> arguments str   (metavar "FILES...")
-    <*> switch          (short 'r' <> long "reorder" <> help "Reorder according to quote accept time")
-
-parse :: Options -> IO ()
-parse o = do
-    let dec = if _reorder o then pcapDecode =$= quoteReorder 300 else pcapDecode
-    evalStateT (CL.sourceList (_files o) $$ pcapOffline =$= dec =$ quotePrinter) PQ.empty
-
-main :: IO ()
-main = do
-    execParser opts >>= parse
-    where
-        opts = info (helper <*> options)
-             (fullDesc <> progDesc "Parse and print quote messages from market data in FILES")
--- Time.hs
+++ Time.hs
@@ -1,28 +1,60 @@
 module Time where
 
-import qualified Data.ByteString.Char8 as BS
+import Data.ByteString.Char8
 import Data.Char
 import Data.Monoid
 
 data Time = Time Int Int Int Int deriving (Show, Eq, Ord)
 
-parseTime :: BS.ByteString -> Time
+-- | Parse bytestring time in HHMMSShh format.
+parseTime :: ByteString -> Time
 parseTime bt = Time (fi h) (fi m) (fi s) (fi hs)
     where
         fi = fromIntegral
-        t = maybe 0 fst $ BS.readInteger bt
+        t = maybe 0 fst $ readInteger bt
         h  = t `div` 1000000 `mod` 100
         m  = t `div`   10000 `mod` 100
         s  = t `div`     100 `mod` 100
         hs =               t `mod` 100
 
-mkTime :: Time -> BS.ByteString
-mkTime (Time h m s hs) = mkDigits 2 h <> mkDigits 2 m <> mkDigits 2 s <> mkDigits 2 hs
+-- | Convert from Unix time
+fromUnixTime :: Integral n => n -> Time
+fromUnixTime t = Time (fi h) (fi m) (fi s) (fi hs)
     where
-        mkDigits 0 _ = BS.empty
-        mkDigits i n = let (d, r) = divMod n 10 in mkDigits (i - 1 :: Int) d <> mkDigit r
+        fi = fromIntegral
+        h  = t `div` 3600000000 `mod` 24
+        m  = t `div`   60000000 `mod` 60
+        s  = t `div`    1000000 `mod` 60
+        hs = t `div`      10000 `mod` 100
 
-        mkDigit = BS.singleton . intToDigit
+-- | Format a 'Time' as HHMMSShh.
+mkTime :: Time -> ByteString
+mkTime (Time h m s hs) = mkDigits h <> mkDigits m <> mkDigits s <> mkDigits hs
+    where
+        mkDigits n = let (t, u) = divMod n 10 in mkDigit t <> mkDigit u
+        mkDigit = singleton . intToDigit
 
+-- | Calculate the different betweeen two 'Time' values in hundredths of a second.
 diffTime :: Time -> Time -> Int
-diffTime (Time ah am as ahs) (Time bh bm bs bhs) = (ah - bh) * 360000 + (am - bm) * 6000 + (as - bs) * 100 + (ahs - bhs)
+diffTime (Time ah am as ahs) (Time bh bm bs bhs) =
+    (ah - bh) * 360000 +
+    (am - bm) * 6000 +
+    (as - bs) * 100 +
+    (ahs - bhs)
+
+-- | Add hundredths of a second to a 'Time'
+addTime :: Time -> Int -> Time
+addTime (Time h m s hs) hundredths =
+    let (s', hs') = divMod (hs + hundredths) 100
+        (m', s'') = divMod (s + s') 60
+        (h', m'') = divMod (m + m') 60
+        h''       = mod    (h + h') 60
+    in  Time h'' m'' s'' hs'
+
+        -- pcapTime p q =
+        --     let t = hdrTime . _pcapHdr $ p
+        --         sec = fromIntegral $ t `div` 1000000 `mod` 60
+        --         hsec = fromIntegral $ t `div` 10000 `mod` 100
+        --         Time h m s _ = parseTime (_acceptTime q)
+        --         (h', m') = (h + (m + 1) `div` 60 , (m + 1) `mod` 60)
+        --     in snd $ min (abs (sec - s), Time h m sec hsec) (abs (60 + sec - s), Time h' m' sec hsec)
